46537f8f29839449843297657f5bdc92fb06261f
Jarosław Zwierz <j20011812@o2.pl>
2024-03-06 23:02:44 +0100

    work on nav

--- client/app/components/auth/login/Login.tsx
+++ client/app/components/auth/login/Login.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+
+import './login.scss'
 
 export default function Login() {
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+  
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    console.log(login, password)
+  }
+
+  function LoginScreen() {
+    return (
+      <div className='login-wrapper'>
+        Login
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder='Login' value={login} onChange={(e) => setLogin(e.target.value)} />
+          <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <LoginScreen />
   )
 }
---
+++ client/app/components/layout/nav/Nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='footer-wrapper'>
+      Nav
+    </div>
+  )
+}
