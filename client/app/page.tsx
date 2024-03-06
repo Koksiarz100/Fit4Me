5c02b1623ed679e160b8dd6cc707ab874a717e59
Jarosław Zwierz <j20011812@o2.pl>
2024-03-06 22:46:44 +0100

    added file structure + starting work on components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+
+import React, { useState } from 'react';
+
+import Login from './components/auth/login/Login';
+import Register from './components/auth/register/Register';
+import Main from './components/main/Main';
+
+import './styles/globals.scss';
+import './styles/page.scss';
+
 export default function Home() {
+  const [screen, setScreen] = useState('main');
+
   return (
-    <main>
-      Test
+    <main className='app-wrapper'>
+      <h1>Fit4Me</h1>
+      <div className='app-nav'>
+        <button onClick={() => setScreen('main')}>Strona główna</button>
+        <button onClick={() => setScreen('login')}>Logowanie</button>
+        <button onClick={() => setScreen('register')}>Rejestracja</button>
+      </div>
+      <div className='app-screen'>
+        {screen === 'login' ? <Login /> : screen === 'register' ? <Register /> : <Main />}
+      </div>
     </main>
   );
 }
