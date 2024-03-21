c5d168e34d5f9f02c21d2fb7e3da8bff71694972
Jarosław Zwierz <jaroslaw.zwierz@student.po.edu.pl>
2024-03-21 18:29:22 +0100

    fixed function with login form

--- client/app/(pages)/login/page.tsx
+++ client/app/(pages)/login/page.tsx
@@ -4,24 +4,33 @@ import React, { useState } from 'react'
 
 import './login.scss'
 
-export default function Login() {
+function loginForm() {
   const [login, setLogin] = useState('')
   const [password, setPassword] = useState('')
-  
-  function handleSubmit(e: React.FormEvent) {
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    console.log(login, password)
+    console.log(login , password)
   }
+
+  return (
+    <>
+      Login
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={login} onChange={e => setLogin(e.target.value)} />
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+          <button type='submit'>Login</button>
+      </form>
+    </>
+  )
+}
+
+export default function Login() {
 
   function LoginScreen() {
     return (
       <div className='login-wrapper'>
-        Login
-        <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='Login' value={login} onChange={(e) => setLogin(e.target.value)} />
-          <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button type='submit'>Login</button>
-        </form>
+        {loginForm()}
       </div>
     )
   }
